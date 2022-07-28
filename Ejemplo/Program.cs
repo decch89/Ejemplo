b15136b976dfcc6ff61c0a0fb894eb34ea6a4a9a
David <dcoreas@ugb.edu.sv>
2022-07-28 11:09:29 -0600

    EjemploV1

---
+++ Ejemplo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int entero;
+            int entero2;
+            int total_suma;
+
+            Console.Write("Ingrese un entero: \n");
+            entero = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese otro entero: \n");
+            entero2 = int.Parse(Console.ReadLine());
+
+            total_suma = entero + entero2/2;
+
+            Console.Write("El total de la suma es: " + total_suma);
+            Console.Read();
+        }
+    }
+}
